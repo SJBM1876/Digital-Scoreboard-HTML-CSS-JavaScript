8177cde78c8eef92a7c5e1f852c675876cbfa89d
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2023-12-12 10:39:30 +0000

    Add files via upload
    A countdown clock has been added that begins when a 'Kick Off' button is clicked. The CSS has been tidied up.

--- footballScoreboardJavaScript.js
+++ footballScoreboardJavaScript.js
@@ -1,5 +1,28 @@
 let homeScore = 0;
 let awayScore = 0;
+let matchDuration = 90 * 60;
+let countdownInterval;
+
+function startCountdown() {
+    if (!countdownInterval) {
+        countdownInterval = setInterval(updateCountdown, 1000);
+    }
+}
+
+
+function updateCountdown() {
+    if (matchDuration > 0) {
+        const minutes = Math.floor(matchDuration / 60);
+        const seconds = matchDuration % 60;
+        document.getElementById('countdown-clock').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        matchDuration--;
+    } else {
+        clearInterval(countdownInterval);
+        countdownInterval = null;
+        document.getElementById('countdown-clock').textContent = "00:00"
+        alert("Full time!");
+    }
+}
 
 function incrementScore(team) {
     if (team === 'home') {
